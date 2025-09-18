8a5a5e88762634028a66f56a45935b43d008a64c
James McCreight <jamesmcc@ucar.edu>
2025-09-18 17:48:53 -0600

    DomainSubset nearly writing prms native files

--- autotest/test_domain_subset.py
+++ autotest/test_domain_subset.py
@@ -1,12 +1,17 @@
 import pathlib as pl
 
 import numpy as np
+import pyPRMS as pp
 import pytest
 import xarray as xr
+from utils import run_prms
 
 import pywatershed as pws
 
-# TODO build a subdomain of a test_domain for a check
+pyprms_meta = pp.MetaData(verbose=False).metadata
+
+# TODO: can we shorten the length of the ucb_2yr run?
+# TODO: remove test nhm code cheats
 
 # NHM domain section for some preliminary testing cheating (to be removed)
 full_domain_dir = pl.Path(
@@ -34,14 +39,26 @@ use_nhm = False
 
 domain_name = "ucb_2yr"
 domain_seg_outlet = {"ucb_2yr": np.int64(44425)}
+# fmt: off
+domain_known_nhm_ids = np.array(
+    [
+        86522, 86621, 86343, 86344, 86525, 86622, 86313, 86333, 86270, 86272,
+        86274, 86278, 86288, 86290, 86283, 86307, 86261, 86263, 86260, 86262,
+        86330, 86337,
+    ]
+)
+# fmt: on
+domain_known_nhm_segs = np.array(
+    [44409, 44412, 44417, 44418, 44420, 44421, 44422, 44423, 44425, 44430]
+)
 
 cbh_types = [("ascii", "netcdf")[1:]]  # until implemented
-output_types = [("pywatershed", "prms")[0:1]]  # until implemented
+output_types = [("pywatershed", "prms")[1:]]  # until implemented
 
 # compound test configs
-subset_styles = ("known", "single_seg")[1:]
-ind_order = ["orig", "shuffle"]
-# id_seg_params = [("known", True), ("known", False), ("single_seg", None)][2:]
+subset_styles = ("known", "single_seg")
+ind_order = ["orig", "shuffle"][0:1]
+
 sub_ids_segs_types = []
 for ss in subset_styles:
     for io in ind_order:
@@ -52,21 +69,69 @@ for ss in subset_styles:
 
 
 @pytest.fixture(scope="function")
-def full_control_file(simulation):
+def full_control_file(simulation, tmp_path):
     if simulation["name"] != f"{domain_name}:nhm":
         pytest.skip("test_domain_subset only runs for {domain_name}:nhm")
+
+    # To keep the test quick, only test for 30 days. We'll re-write the control
+    # file to temp_path but we have to maintain the parameter and CBH file
+    # info. (Since we are using pyPRMS, only works for the case of a single
+    # param_file.)
+    control_file = simulation["control_file"]
+    control_parent = control_file.parent.resolve()
+    new_control_file = tmp_path / "shortend.control"
+
+    control = pws.utils.utils.pyprms_control_no_defaults(
+        control_file, metadata=pyprms_meta, verbose=False
+    )
+    cv = control.control_variables
+    # edit time
+    cv["end_time"].values = cv["start_time"].values + np.timedelta64(30, "D")
+    # param_file path
+    param_file = pl.Path(cv["param_file"].values)
+    if not param_file.is_absolute():
+        cv["param_file"].values = str(control_parent / param_file)
+    # cbh file paths
+    # This is such a mess. Because pyPRMS loads defaults into unpopulated
+    # fields, it's a very difficult proposition to tell if any CBH file is
+    # actually active. Because a cbh file being active in PRMS could be
+    # indicated a flag or a module choice, the logic for detecting what files
+    # are actually active is a mess. pyPRMS pull #40 (which dosent insert
+    # default values when none are supplied) would hugely simplify this but
+    # it is not merged let alone on pypi. What we'll do is test if the file
+    # exists, if it does not we'll delete it from the control. But this is
+    # will cause input errors to propigate by dropping the files rather than
+    # being caught by FileNotFound-like errors down the line.
+    # The other option would be map from cbh file keys to flags or modules
+    # to test... something for another day.
+    for kk, vv in pws.constants.cbh_ctl_var_map.items():
+        if kk in cv.keys() and cv[kk].values:
+            cbh_file_name = pl.Path(cv[kk].values)
+            if not cbh_file_name.is_absolute():
+                cv[kk].values = str(control_parent / cbh_file_name)
+
+            if not pl.Path(cv[kk].values).exists():
+                control.remove(kk)
+                # warn? gosh, not sure
+            else:
+                print(f"{kk}= {cv[kk].values}")
+                pass
+
+    control.write(new_control_file)
 
     if use_nhm:
         return full_control_file_nhm
     else:
-        return simulation["control_file"]
+        return new_control_file
 
 
 @pytest.fixture(scope="function", params=cbh_types)
 def full_cbh_nc_file_dict(simulation, request):
     cbh_type = request.param[0]
     if cbh_type == "netcdf":
-        full_cbh_dir = simulation["control_file"].parent
+        # for domains in test_data, the netcdf cbh files are found in
+        # simulation["dir"]
+        full_cbh_dir = simulation["dir"]
         full_cbh_nc_file_dict = {
             kk: full_cbh_dir / vv
             for kk, vv in {
@@ -137,7 +202,7 @@ def test_pws_subset_known_ids_segs(
 
     # can test that output_format=None raises error
     sub_domain_dir = tmp_path / "subdomain"
-    subdomain.write(write_dir=sub_domain_dir, output_format="pywatershed")
+    subdomain.write(write_dir=sub_domain_dir, output_format=output_format)
 
     if output_format.lower() == "pywatershed":
         # test NHM
@@ -157,9 +222,15 @@ def test_pws_subset_known_ids_segs(
         full_ctl = pws.Control.load_prms(
             full_control_file, warn_unused_options=False
         )
-        full_param_file = full_dir / full_ctl.options["parameter_file"]
+        full_param_file = pl.Path(full_ctl.options["parameter_file"])
+        if not full_param_file.is_absolute():
+            full_param_file = full_dir / str(full_param_file)
         full_params = pws.parameters.PrmsParameters.load(full_param_file)
-        full_ctl.options["input_dir"] = full_dir
+        input_dirs = np.array(
+            [str(dd.parent) for dd in full_cbh_nc_file_dict.values()]
+        )
+        assert (input_dirs == input_dirs[0]).all()
+        full_ctl.options["input_dir"] = str(input_dirs[0])
         full_ctl.options["netcdf_output_dir"] = pl.Path(full_run_dir)
         full_model = pws.Model(
             nhm_proc_list, control=full_ctl, parameters=full_params
@@ -196,7 +267,8 @@ def test_pws_subset_known_ids_segs(
                 raise ValueError(f"Comparison failed for {var_name=}")
 
     elif output_format.lower() == "prms":
-        pass
+        control_file = list(sub_domain_dir.glob("*.control"))[0]
+        run_prms(control_file)
 
     else:
         raise ValueError("How'd we get here?")
--- pywatershed/utils/domain_subset.py
+++ pywatershed/utils/domain_subset.py
@@ -12,46 +12,45 @@ from .segment_from_tracing import (
     get_nhm_segs_ids_above_seg,
 )
 
+pyprms_meta = pp.MetaData(verbose=False).metadata
+
+# TODO: revisit subset parameters now that isel is being used, may be much
+#       simpler
 # TODO: subset restarts
+# TODO: maybe something todo, there are no real checks on time or subsetting
+#       in time.
+
+additional_cbh_meta = {
+    "PET_cbh_file": {
+        "datatype": "string",
+        "description": (
+            "Pathname of the CBH file of pre-processed potential "
+            "evapotransipration input data for each HRU to specify "
+            "variable potet."
+        ),
+        "context": "scalar",
+        # "default": np.str_(""),
+    },
+    "AET_cbh_file": {
+        "datatype": "string",
+        "description": (
+            "Pathname of the CBH file of pre-processed actual "
+            "evapotransipration input data for each HRU to specify "
+            "variable actet."
+        ),
+        "context": "scalar",
+        # "default": np.str_("tmin.day"),
+    },
+}
 
 """Notes:
 At the moment the assumption is that the full domain is provided PRMS input
 files with only the option for NetCDF files.
 """
 
-cbh_control_names = [
-    "albedo_day",
-    "cloud_cover_day",
-    "humidity_day",
-    "potet_day",
-    "precip_day",
-    "swrad_day",
-    "tmax_day",
-    "tmin_day",
-    "transp_day",
-    "windspeed_day",
-    "AET_cbh_file",
-    "PET_cbh_file",
-    "rhavg",
-]
-# it's vague to me what these are in the headers of the files for
-# https://github.com/DOI-USGS/pyPRMS/blob/49cbb8cd46b6760b1be67c106b8074688abaab39/tests/func/test_Control/ctl_metadata_default.csv#L96
-cbh_var_names = [
-    "albedo_hru",
-    "cloud_cover_cbh",
-    "humidity_hru",
-    "potet",
-    "prcp",
-    "swrad",  # ?
-    "tmax",
-    "tmin",
-    "transp_on",  # ?
-    "windspeed_hru",
-    "actet",
-    "potet",
-    "rhavg",
-]
-cbh_ctl_var_map = dict(zip(cbh_control_names, cbh_var_names))
+cbh_ctl_var_map = pws.constants.cbh_ctl_var_map
+cbh_control_names = list(cbh_ctl_var_map.keys())
+cbh_var_names = list(cbh_ctl_var_map.values())
 
 
 class DomainSubset:
@@ -59,6 +58,8 @@ class DomainSubset:
 
     Args:
         full_control_file: A pathlib.Path to the full domain control file.
+          Currently, the "param_file" field must a be a scalar (multiple
+          parameter files specified will throw an error).
         sub_nhm_ids: Either an np.ndarray of the nhm_ids or None if subsetting
           above a single nhm_seg.
         sub_nhm_segs: Either an np.ndarray of the nhm_segs or a np.int64 if
@@ -79,7 +80,7 @@ class DomainSubset:
         full_control_file: pl.Path,
         sub_nhm_ids: Union[np.ndarray, None],
         sub_nhm_segs: Union[np.ndarray, np.int64],
-        full_cbh_nc_files_dict: dict[str, pl.Path],
+        full_cbh_nc_files_dict: Union[dict[str, pl.Path], None],
         start_one_below_nhm_seg: bool = False,
         output_format: Literal["pywatershed", "PRMS", None] = None,
         from_seg_calc_parallel: bool = False,
@@ -108,11 +109,17 @@ class DomainSubset:
         self._output_format = output_format
         self._from_seg_calc_parallel = from_seg_calc_parallel
         self._from_seg_calc_check = from_seg_calc_check
-
         # Get the full domain parameters
         self._full_control = pws.Control.load_prms(
             self._full_control_file, warn_unused_options=False
         )
+        param_file = self._full_control.options["parameter_file"]
+        if not isinstance(param_file, str) and len(param_file) > 1:
+            raise NotImplementedError(
+                "DomainSubset only works with single parameter files at the "
+                "moment. Please consolidate your parameter files or extend "
+                "pyPRMS to handle multiple parameter files."
+            )
 
         if full_cbh_nc_files_dict is None:
             raise NotImplementedError(
@@ -129,8 +136,10 @@ class DomainSubset:
 
         self._get_full_params()
         # Solve upstream segs and hrus if only a single segment is specified.
+
         if self._sub_nhm_ids is None:
             self._sub_domain_from_nhm_seg()
+
         # <
         self._set_subset_masks_order()
         self._subset_cbh()
@@ -200,9 +209,10 @@ class DomainSubset:
         return None
 
     def _get_from_segment_params(
-        self, self_attr_key: str, parallel=True, check=True
+        self, self_attr_key: str, parallel=False, check=True
     ) -> None:
         params_edit = getattr(self, self_attr_key)
+        print("starting get_from_segment_params")
         from_dict = get_from_segment_params(
             tosegment=params_edit.tosegment.values,
             parallel=parallel,
@@ -287,14 +297,14 @@ class DomainSubset:
         # bit of a silly pipeline, dont see another way to rename dim
         # independently of the coordinate with a dataarray
         self._sub_cbh_files_dict = {
-            kk: xr.open_dataset(vv).rename_dims(
+            kk: xr.load_dataset(vv).rename_dims(
                 nhm_id="nhru"
             )  # .to_dataarray()
             for kk, vv in self._full_cbh_nc_files_dict.items()
         }
         for kk in self._sub_cbh_files_dict.keys():
-            self._sub_cbh_files_dict[kk] = self._sub_cbh_files_dict[kk].where(
-                self._sub_nhm_ids_mask, drop=True
+            self._sub_cbh_files_dict[kk] = self._sub_cbh_files_dict[kk].isel(
+                nhru=self._sub_nhm_ids_mask
             )
             assert (
                 self._sub_cbh_files_dict[kk].nhm_id.values
@@ -316,7 +326,10 @@ class DomainSubset:
         # drop on hrus and segments separately, we separate all these dims that
         # are not time. the we reassemble
         params_sub = self._full_params.copy()
-        params_sub = params_sub.drop_vars(indexed_vars)
+        indexed_vars_in_ds = set(indexed_vars).intersection(
+            set(params_sub.variables)
+        )
+        params_sub = params_sub.drop_vars(indexed_vars_in_ds)
         all_dims_drop = set(
             [
                 "nhru",
@@ -332,11 +345,11 @@ class DomainSubset:
                 all_dims_drop - set([gg])
             )
 
-        grouped_param_subs["nhru"] = grouped_param_subs["nhru"].where(
-            self._sub_nhm_ids_mask, drop=True
+        grouped_param_subs["nhru"] = grouped_param_subs["nhru"].isel(
+            nhru=self._sub_nhm_ids_mask
         )
-        grouped_param_subs["nsegment"] = grouped_param_subs["nsegment"].where(
-            self._sub_nhm_segs_mask, drop=True
+        grouped_param_subs["nsegment"] = grouped_param_subs["nsegment"].isel(
+            nsegment=self._sub_nhm_segs_mask
         )
 
         self._sub_params = xr.merge(grouped_param_subs.values())
@@ -439,32 +452,45 @@ class DomainSubset:
                 "output_format not specified on initialization or write."
             )
 
-        pyprms_meta = pp.MetaData(verbose=False).metadata
-        self._sub_control = pp.ControlFile(
+        self._sub_control = pws.utils.utils.pyprms_control_no_defaults(
             self._full_control_file, metadata=pyprms_meta, verbose=False
+        )
+        self._sub_control_file_name = (
+            f"{self._full_control_file.stem}_subset.control"
         )
 
         if output_format.lower() == "pywatershed":
             self._write_pws(write_dir=write_dir)
         elif output_format.lower() == "prms":
-            raise ValueError("PRMS output format not yet implemented.")
-
             self._write_prms(write_dir=write_dir)
-        pass
+
+        return None
 
     def _write_prms(self, write_dir: pl.Path) -> None:
-        # write ascii cbh files
-        # write ascii parameter file
-        # write control file
-        pass
+        # these functions set the control, reusing the filename in it
+        # but making it relative to the write_dir.
+        self._cbh_dataset_to_ascii(write_dir=write_dir)
+        self._parameters_to_ascii(write_dir=write_dir)
+
+        self._sub_control.write(write_dir / self._sub_control_file_name)
+
+        return None
 
     def _write_pws(self, write_dir: pl.Path) -> None:
         # Edit the control
         # dont really need to do anything with the cbh or param files for
         # pywatershed, but since the control file really shouldnt be used
         # by PRMS, we'll edit it to point to the netcdf files.
+
         for kk in self._sub_cbh_files_dict.keys():
-            self._sub_control.control_variables[kk].values = f"{kk}.nc"
+            if kk in self._sub_control.control_variables.keys():
+                self._sub_control.control_variables[kk].values = f"{kk}.nc"
+            else:
+                vv = pp.ControlVariable(
+                    name=kk, strict=False, meta=additional_cbh_meta[kk]
+                )
+                vv.__dict__["_ControlVariable__values"] = f"{kk}.nc"
+                self._sub_control.control_variables[kk] = vv
 
         self._sub_control.control_variables[
             "param_file"
@@ -472,6 +498,7 @@ class DomainSubset:
 
         # write netcdf cbh files
         for kk, vv in self._sub_cbh_files_dict.items():
+            print(f"{kk=}")
             # check the order before output
             assert (vv.nhm_id.values == self._sub_nhm_ids_order).all()
             # The naming of cbh files, their control parameters, and their
@@ -494,6 +521,107 @@ class DomainSubset:
         self._sub_params.to_netcdf(write_dir / param_file_name)
 
         # write control file
-        ctl_file_name = f"{self._full_control_file.stem}_subset.control"
-        self._sub_control.write(write_dir / ctl_file_name)
+        self._sub_control.write(write_dir / self._sub_control_file_name)
+        return None
+
+    def _cbh_dataset_to_ascii(self, write_dir):
+        test_data_dir = pl.Path(
+            pws.constants.__pywatershed_root__ / "../test_data"
+        )
+        # simply load a dummy CBH netcdf file from the pywatershed repo
+        # and replace its dataset with ours.
+        pp_cbh = pp.Cbh(
+            src_path=test_data_dir / "drb_2yr/cbh.nc",
+            metadata=pyprms_meta,
+            engine="netcdf",
+        )
+        cbh_ds = xr.merge(self._sub_cbh_files_dict.values())
+        pp_cbh._Cbh__dataset = cbh_ds
+        for kk, vv in self._sub_cbh_files_dict.items():
+            var_name = list(vv.data_vars)[0]
+            file_name = pl.Path(self._sub_control[kk].values).name
+            self._sub_control[kk].values = file_name
+            pp_cbh.write_ascii(
+                filename=write_dir / file_name, variable=var_name
+            )
+
+    def _parameters_to_ascii(self, write_dir):
+        # This is basically implemeting pyPRMS/parameters/ParameterNetCDF.py
+        # but just skipping reading from netcdf file
+        verbose = False
+
+        # prep the parameters to be acceptable
+        sub_params_ds = self._sub_params.rename(
+            ndoy="ndays", scalar="one", nmonth="nmonths"
+        )
+        drop_vars = [
+            "doy",
+            "from_segment_starts",
+            "from_segment_ends",
+            "from_nhm_seg_unstruct",
+            "from_segment",
+            "hru_in_to_cf",
+        ]
+        sub_params_ds = sub_params_ds.drop(drop_vars)
+
+        pp_params = pp.Parameters(metadata=pyprms_meta, verbose=verbose)
+        pp_params.__verbose = verbose
+
+        # leaving this here for the mask_and_scale and decode_timedelta
+        # implications
+        # xr_df = xr.open_dataset(
+        #     self.__filename, mask_and_scale=False, decode_timedelta=False
+        # )
+
+        # Populate the dimensions first
+        pp_params.dimensions.add(name="one", size=1)
+        # self.dimensions.add(name='ndays')
+
+        for dn, ds in dict(sub_params_ds.sizes).items():
+            pp_params.dimensions.add(name=str(dn), size=ds)
+
+        # Add ndepl using ndeplval
+        pp_params.dimensions.add(
+            name="ndepl", size=int(sub_params_ds.sizes["ndeplval"] / 11)
+        )
+
+        # Add nobs if needed
+        if not pp_params.dimensions.exists("nobs"):
+            if pp_params.dimensions.exists("npoigages"):
+                pp_params.dimensions.add(
+                    name="nobs",
+                    size=pp_params.dimensions.get("npoigages").size,
+                )
+            else:
+                pp_params.dimensions.add(name="nobs", size=0)
+
+        if not pp_params.dimensions.exists("ngw"):
+            pp_params.dimensions.add(
+                name="ngw", size=pp_params.dimensions.get("nhru").size
+            )
+
+        if not pp_params.dimensions.exists("nssr"):
+            pp_params.dimensions.add(
+                name="nssr", size=pp_params.dimensions.get("nhru").size
+            )
+
+        # Now add the parameters
+        for var in sub_params_ds.variables.keys():
+            if pp_params.__verbose:
+                print(str(var))
+
+            cparam = sub_params_ds[var].T
+
+            # Add the parameter
+            pp_params.add(name=str(var))
+
+            # Add the data
+            pp_params.get(str(var)).data = cparam.values
+
+        pp_params.adjust_bounded_parameters()
+
+        file_name = pl.Path(self._sub_control["param_file"].values).name
+        self._sub_control["param_file"].values = file_name
+        pp_params.write_parameter_file(filename=write_dir / file_name)
+
         return None
