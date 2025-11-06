9305bc3dc9cad982ed67efad90682e27551f54f7
James McCreight <jamesmcc@ucar.edu>
2025-11-05 18:48:46 -0700

    prms 5.2.1.1 double precision compile with G0 output same as prms 5.2.1, some variables not output at full resolution

--- prms_src/prms5.2.1.1/prms/nhru_summary.f90
+++ prms_src/prms5.2.1.1/prms/nhru_summary.f90
@@ -22,7 +22,7 @@
 ! Parameters
       INTEGER, SAVE, ALLOCATABLE :: Nhm_id(:)
 ! Control Parameters
-      INTEGER, SAVE :: NhruOutVars, NhruOut_freq, NhruOut_format, NhruOutNcol, outputSelectDatesON_OFF 
+      INTEGER, SAVE :: NhruOutVars, NhruOut_freq, NhruOut_format, NhruOutNcol, outputSelectDatesON_OFF
       CHARACTER(LEN=36), SAVE, ALLOCATABLE :: NhruOutVar_names(:)
       CHARACTER(LEN=MAXFILE_LENGTH), SAVE :: NhruOutBaseFileName, selectDatesFileName
       END MODULE PRMS_NHRU_SUMMARY
@@ -288,14 +288,14 @@
  9002 FORMAT ('("Date"',I0,'('', ''I0))')
  9003 FORMAT ('(I4,', I0,'('','',ES10.3))')
  9004 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',I0))')
- 9005 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',F0.4))')
- 9006 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',F0.3))')
- 9007 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',F0.2))')
+ 9005 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',G0))')
+ 9006 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',G0))')
+ 9007 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',G0))')
  9008 FORMAT ('(I4,', I0,'('','',F0.4))')
  9009 FORMAT ('(I4,', I0,'('','',F0.3))')
  9010 FORMAT ('(I4,', I0,'('','',F0.2))')
  9011 FORMAT ('(I4,', I0,'('','',F0.5))')
- 9012 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',F0.5))')
+ 9012 FORMAT ('(I4, 2(''-'',I2.2),',I0,'('','',G0))')
  9013 FORMAT ('(I4, 2(''-'',I2.2))')
  9014 FORMAT ('(',I0,'(ES10.3,'',''),ES10.3)')
  9015 FORMAT ('(',I0,'(F0.4,'',''),F0.4)')
